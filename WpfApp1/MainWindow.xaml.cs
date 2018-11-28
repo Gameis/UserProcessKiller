379e36720f40cfe75cd11a04a3147ccc2997d2ad
nethermore11 <nethermore11@gmail.com>
2018-11-28 18:41:33 +0700

    Частично добавлена низкоуровневая обработка нажатия клавишь

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Threading;
 
 namespace WpfApp1
@@ -25,7 +27,106 @@ namespace WpfApp1
 			Closing += OnClosingWindow;
 		}
 
-		private void OnClosingWindow(object sender, CancelEventArgs e) {
+		private const uint SPI_GETFILTERKEYS = 0x0032;
+		private const uint SPI_SETFILTERKEYS = 0x0033;
+		private const uint SPI_GETTOGGLEKEYS = 0x0034;
+		private const uint SPI_SETTOGGLEKEYS = 0x0035;
+		private const uint SPI_GETSTICKYKEYS = 0x003A;
+		private const uint SPI_SETSTICKYKEYS = 0x003B;
+
+		private static bool      StartupAccessibilitySet = false;
+		private static SKEY      StartupStickyKeys;
+		private static SKEY      StartupToggleKeys;
+		private static FILTERKEY StartupFilterKeys;
+
+		private const uint SKF_STICKYKEYSON  = 0x00000001;
+		private const uint TKF_TOGGLEKEYSON  = 0x00000001;
+		private const uint SKF_CONFIRMHOTKEY = 0x00000008;
+		private const uint SKF_HOTKEYACTIVE  = 0x00000004;
+		private const uint TKF_CONFIRMHOTKEY = 0x00000008;
+		private const uint TKF_HOTKEYACTIVE  = 0x00000004;
+		private const uint FKF_CONFIRMHOTKEY = 0x00000008;
+		private const uint FKF_HOTKEYACTIVE  = 0x00000004;
+		private const uint FKF_FILTERKEYSON  = 0x00000001;
+
+		[StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+		public struct FILTERKEY
+		{
+			public uint cbSize;
+			public uint dwFlags;
+			public uint iWaitMSec;
+			public uint iDelayMSec;
+			public uint iRepeatMSec;
+			public uint iBounceMSec;
+		}
+
+		[StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+		public struct SKEY
+		{
+			public uint cbSize;
+			public uint dwFlags;
+		}
+
+        private static uint SKEYSize = sizeof(uint) * 2;
+		private static uint FKEYSize = sizeof(uint) * 6;
+		[DllImport("user32.dll", EntryPoint = "SystemParametersInfo", SetLastError = false)]
+		private static extern bool SystemParametersInfo(uint          action, uint param,
+														ref FILTERKEY vparam, uint init);
+
+        public static void AllowAccessibilityShortcutKeys(bool bAllowKeys)
+        {
+            if (!StartupAccessibilitySet)
+            {
+                StartupStickyKeys.cbSize = SKEYSize;
+                StartupToggleKeys.cbSize = SKEYSize;
+                StartupFilterKeys.cbSize = FKEYSize;
+                SystemParametersInfo(SPI_GETSTICKYKEYS, SKEYSize, ref StartupStickyKeys, 0);
+                SystemParametersInfo(SPI_GETTOGGLEKEYS, SKEYSize, ref StartupToggleKeys, 0);
+                SystemParametersInfo(SPI_GETFILTERKEYS, FKEYSize, ref StartupFilterKeys, 0);
+                StartupAccessibilitySet = true;
+            }
+
+            if (bAllowKeys)
+            {
+                // Restore StickyKeys/etc to original state and enable Windows key 
+                SystemParametersInfo(SPI_SETSTICKYKEYS, SKEYSize, ref StartupStickyKeys, 0);
+                SystemParametersInfo(SPI_SETTOGGLEKEYS, SKEYSize, ref StartupToggleKeys, 0);
+                SystemParametersInfo(SPI_SETFILTERKEYS, FKEYSize, ref StartupFilterKeys, 0);
+            }
+            else
+            {
+                // Disable StickyKeys/etc shortcuts but if the accessibility feature is on,  
+                // then leave the settings alone as its probably being usefully used 
+                SKEY skOff = StartupStickyKeys;
+                if ((skOff.dwFlags & SKF_STICKYKEYSON) == 0)
+                {
+                    // Disable the hotkey and the confirmation 
+                    skOff.dwFlags &= ~SKF_HOTKEYACTIVE;
+                    skOff.dwFlags &= ~SKF_CONFIRMHOTKEY;
+                    SystemParametersInfo(SPI_SETSTICKYKEYS, SKEYSize, ref skOff, 0);
+                }
+                SKEY tkOff = StartupToggleKeys;
+                if ((tkOff.dwFlags & TKF_TOGGLEKEYSON) == 0)
+                {
+                    // Disable the hotkey and the confirmation 
+                    tkOff.dwFlags &= ~TKF_HOTKEYACTIVE;
+                    tkOff.dwFlags &= ~TKF_CONFIRMHOTKEY;
+                    SystemParametersInfo(SPI_SETTOGGLEKEYS, SKEYSize, ref tkOff, 0);
+                }
+
+                FILTERKEY fkOff = StartupFilterKeys;
+                if ((fkOff.dwFlags & FKF_FILTERKEYSON) == 0)
+                {
+                    // Disable the hotkey and the confirmation 
+                    fkOff.dwFlags &= ~FKF_HOTKEYACTIVE;
+                    fkOff.dwFlags &= ~FKF_CONFIRMHOTKEY;
+                    SystemParametersInfo(SPI_SETFILTERKEYS, FKEYSize, ref fkOff, 0);
+                }
+            }
+        }
+
+
+        private void OnClosingWindow(object sender, CancelEventArgs e) {
 			hooks.ShowStartMenu();
 			hooks.EnableCTRLALTDEL();
 		}
@@ -61,5 +162,9 @@ namespace WpfApp1
 		private void SomeMethod_OnClick(object sender, RoutedEventArgs e) {
 			hooks.SomeMethod();
         }
+
+		private void MainWindow_OnKeyDown(object sender, KeyEventArgs e) {
+			throw new NotImplementedException();
+		}
 	}
 }
